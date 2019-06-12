ca84c5f6dc02329c29df0d8e0e262a761acf75e8
slavic-sky <zirchik1997@mail.ru>
2019-06-12 04:37:44 +0300

    done 'till QA page

---
+++ src/test/java/Main.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
+
---
+++ src/test/java/TestWrike.java
@@ -0,0 +1,46 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.swing.text.Document;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+
+public class TestWrike{
+
+    @Test
+    public void firstTest() {
+
+        System.setProperty("webdriner.chrome.driver", "/chromedriver");
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://www.wrike.com");
+        driver.manage().window().maximize();
+
+        WebElement getJoinButton = driver.findElement(By.xpath(".//div[@class=\"r\"]/form/" +
+                "button[@class=\"wg-header__free-trial-button wg-btn wg-btn--green\"]"));
+        getJoinButton.click();
+
+        String handle = driver.getWindowHandle();
+        driver.switchTo().window(handle);
+
+        WebElement email = driver.findElement(By.xpath(".//label[@class=\"modal-form-trial__label\"]/input"));
+        email.sendKeys(StringOperation.generateRandomString(3) + "wpt@wriketask.qaa");
+
+        WebElement getCreateButton = driver.findElement(By.xpath(".//label[@class=\"modal-form-trial__label\"]/button"));
+        //String startURL = driver.getCurrentUrl();
+        getCreateButton.click();
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.urlMatches("https://www.wrike.com/resend/"));
+
+        //WebElement getQAButton = driver.findElement(By.xpath)
+    }
+}
+
